639c8b9bace7099cb52940303bf6714c7d090a51
xinglan <xinglan_liu@foxmail.com>
2021-02-05 16:46:51 +0800

    Test Astar Queue functionality

--- unitTests.py
+++ unitTests.py
@@ -1,9 +1,11 @@
 import unittest
 from tests import abstractNodeTest
+from tests import aStarQueueTest
 
 if __name__ == '__main__':
     suite = unittest.TestSuite()
-    suite.addTests(unittest.TestLoader().loadTestsFromModule(abstractNodeTest))
+    suite.addTest(unittest.TestLoader().loadTestsFromModule(abstractNodeTest))
+    suite.addTest(unittest.TestLoader().loadTestsFromModule(aStarQueueTest))
 
     runner = unittest.TextTestRunner(verbosity=2)
     runner.run(suite)
