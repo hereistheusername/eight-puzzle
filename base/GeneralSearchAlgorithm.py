9ab1f741573de66fd699af8189778e13906f67d4
xinglan <xinglan_liu@foxmail.com>
2021-02-06 21:50:14 +0800

    add context to GeneralSearchAlgorithm

--- base/GeneralSearchAlgorithm.py
+++ base/GeneralSearchAlgorithm.py
@@ -1,10 +1,15 @@
-def GeneralSearchAlgorithm(problem, queue):
+# ctx is context, which is used to control output message when expanding node or finding the goal
+def GeneralSearchAlgorithm(problem, queue, ctx):
     nodes = queue(problem.get_initial_state())
 
     while True:
         if nodes.isEmpty():
             return ("failure")
         node = nodes.remove_front()
+        # expanding node
+        ctx.onExpand(node)
         if problem.goal_test(node.get_current_state()):
+            # found the goal
+            ctx.onSuccess(node)
             return ("success", node)
         nodes.queueing(node.expand(problem.operators))
