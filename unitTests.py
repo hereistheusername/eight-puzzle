288e2451ab7843b4c7e3ea174b54f1430298eab0
xinglan <xinglan_liu@foxmail.com>
2021-02-06 15:52:14 +0800

    Test Astar Operators

--- unitTests.py
+++ unitTests.py
@@ -2,12 +2,14 @@ import unittest
 from tests import abstractNodeTest
 from tests import aStarQueueTest
 from tests import aStarProblemTest
+from tests import aStarOperatorsTest
 
 if __name__ == '__main__':
     suite = unittest.TestSuite()
-    suite.addTest(unittest.TestLoader().loadTestsFromModule(abstractNodeTest))
-    suite.addTest(unittest.TestLoader().loadTestsFromModule(aStarQueueTest))
-    suite.addTest(unittest.TestLoader().loadTestsFromModule(aStarProblemTest))
+    suite.addTests(unittest.TestLoader().loadTestsFromModule(abstractNodeTest))
+    suite.addTests(unittest.TestLoader().loadTestsFromModule(aStarQueueTest))
+    suite.addTests(unittest.TestLoader().loadTestsFromModule(aStarProblemTest))
+    suite.addTests(unittest.TestLoader().loadTestsFromModule(aStarOperatorsTest))
 
     runner = unittest.TextTestRunner(verbosity=2)
     runner.run(suite)
