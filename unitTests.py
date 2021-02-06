d73163f8b0f38531206f75bbef64ce5f85588ca4
xinglan <xinglan_liu@foxmail.com>
2021-02-06 21:15:27 +0800

    Test Astar Node

--- unitTests.py
+++ unitTests.py
@@ -3,6 +3,7 @@ from tests import abstractNodeTest
 from tests import aStarQueueTest
 from tests import aStarProblemTest
 from tests import aStarOperatorsTest
+from tests import aStarNodeTest
 
 if __name__ == '__main__':
     suite = unittest.TestSuite()
@@ -10,6 +11,7 @@ if __name__ == '__main__':
     suite.addTests(unittest.TestLoader().loadTestsFromModule(aStarQueueTest))
     suite.addTests(unittest.TestLoader().loadTestsFromModule(aStarProblemTest))
     suite.addTests(unittest.TestLoader().loadTestsFromModule(aStarOperatorsTest))
+    suite.addTests(unittest.TestLoader().loadTestsFromModule(aStarNodeTest))
 
     runner = unittest.TextTestRunner(verbosity=2)
     runner.run(suite)
